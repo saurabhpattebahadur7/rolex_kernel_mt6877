30cdc00b35fe125c7d111b94fdb6b0d0897e7bc3
Rolex040623 <saurabhpattebahadur7@gmail.com>
2026-06-14 15:59:00 +0530

    Adjust scheduling tunables and granularity values
    Updated scheduling parameters for better performance.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -84,15 +84,15 @@ unsigned int sysctl_sched_cstate_aware = 1;
  *
  * (default SCHED_TUNABLESCALING_LOG = *(1+ilog(ncpus))
  */
-enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
+enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity		= 300000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 300000ULL;
+unsigned int sysctl_sched_min_granularity = 3000000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 3000000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
@@ -114,10 +114,10 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  *
  * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_wakeup_granularity		= 400000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity	= 400000UL;
+unsigned int sysctl_sched_wakeup_granularity = 4000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity = 4000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost	= 100000UL;
+const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
 #ifdef CONFIG_SCHED_WALT
 unsigned int sysctl_sched_use_walt_cpu_util = 1;
