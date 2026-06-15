0fe7c637225c221c60f243c3a90eaf9d3a5f9604
Rolex040623 <saurabhpattebahadur7@gmail.com>
2026-06-15 16:47:23 +0530

    Update GPU frequency configuration settings

--- drivers/misc/mediatek/base/power/include/gpufreq_v1/mt6877/mtk_gpufreq_internal_plat.h
+++ drivers/misc/mediatek/base/power/include/gpufreq_v1/mt6877/mtk_gpufreq_internal_plat.h
@@ -28,8 +28,8 @@
  * (0, 0) -> DVFS disable
  **************************************************/
 #define MT_GPUFREQ_DVFS_ENABLE          1
-#define MT_GPUFREQ_CUST_CONFIG          0
-#define MT_GPUFREQ_CUST_INIT_OPP        (g_opp_table_segment_1[16].gpufreq_khz)
+#define MT_GPUFREQ_CUST_CONFIG          1
+#define MT_GPUFREQ_CUST_INIT_OPP        (g_opp_table_segment_1[0].gpufreq_khz)
 
 /**************************************************
  * DVFS Setting
