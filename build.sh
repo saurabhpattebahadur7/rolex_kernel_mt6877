f092117d6e65aeae916d2c828abd5cc50d2d0e1e
Rolex040623 <saurabhpattebahadur7@gmail.com>
2026-08-04 08:34:47 +0530

    Rename ZIP file for kernel build

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ DATE=$(date '+%Y%m%d-%H%M')
 
 DEVICE="${1:-pissarro}"
 DEFCONFIG="${DEVICE}_defconfig"
-ZIPNAME="BrickedKernel-${DEVICE}-${DATE}.zip"
+ZIPNAME="RolexKernel-${DEVICE}-${DATE}.zip"
 
 echo -e "Building for: $DEVICE\n"
 
